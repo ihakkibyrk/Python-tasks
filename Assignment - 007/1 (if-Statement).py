58e665ce53b89107be667298985d763d443d9ded
ihakkibyrk <75913024+ihakkibyrk@users.noreply.github.com>
2021-01-13 23:47:00 +0100

    Create 1 (if-Statement).py

---
+++ Assignment - 007/1 (if-Statement).py
@@ -0,0 +1,8 @@
+name = input("write your name : ")
+
+original_name = 'ibrahim'
+
+if name == original_name:
+    print("Hello, ibrahim! The password is : uNhugM!%")
+else:
+    print(f'Hello, {name}! See you later')
